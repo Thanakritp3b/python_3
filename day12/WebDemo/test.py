dab96796969fa8de51b3c633e1ac660b1bf1ca79
Thanakrit Pongtanawannagon <thanakrit03b@gmail.com>
2025-01-28 10:11:10 +0700

    hmmm

--- day12/WebDemo/test.py
+++ day12/WebDemo/test.py
@@ -7,7 +7,6 @@ def hello_world():
     return "<p>Go to path</p>"
 
 
-@app.route("/<id1>")
-def test_square(id1):
-    x=id1.split(',')
-    return f"<p>Answer is {int(x[0])**2 + int(x[1])**2}<p>"
+@app.route("/<int:id1>/<int:id2>")
+def test_square(id1, id2):
+    return f"<p>Answer is { id1 ** 2 + id2 ** 2 }<p>"
