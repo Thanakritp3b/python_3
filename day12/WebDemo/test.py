19d7bfc8e7cce6ad542064f97439f00905db18e2
Thanakrit Pongtanawannagon <thanakrit03b@gmail.com>
2025-01-28 11:50:47 +0700

    flask demo

--- day12/WebDemo/test.py
+++ day12/WebDemo/test.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Go to path</p>"
+def home():
+    return render_template('index.html')
 
-
-@app.route("/<int:id1>/<int:id2>")
-def test_square(id1, id2):
-    return f"<p>Answer is { id1 ** 2 + id2 ** 2 }<p>"
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    number = int(request.form['number'])
+    result = number * number
+    return render_template('index.html', number=number, result=result)
